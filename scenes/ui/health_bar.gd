608f467499b350fb200623c07a7633bcbe22f9cf
ollulf <>
2025-11-19 22:38:00 +0100

    pushed not working code sorry

--- scenes/ui/health_bar.gd
+++ scenes/ui/health_bar.gd
@@ -2,7 +2,8 @@ extends Control
 class_name HealthBar
 
 func _connect_to_signal(_signal : Signal):
-	_signal.connect(_on_took_damage())
+	#_signal.connect(_on_took_damage())
+	return
 
 func _on_took_damage():
 	_update_healthbar()
