d5653150d007a5656873f383428b1cca61ec6c8c
LucasBoe <lucas@lumotex.de>
2025-11-19 19:36:21 +0100

    ADDED: full game loop incl. unloading and placement reset

--- scenes/placement_handler.gd
+++ scenes/placement_handler.gd
@@ -2,6 +2,8 @@ extends Node3D
 class_name PlacementHandler
 
 @onready var canvas = %CanvasLayer
+@onready var inactive_holder = $InactiveObjectHolder
+@onready var active_holder = $ActiveObjectHolder
 
 var active = false
 var button : Button
@@ -9,12 +11,14 @@ var button : Button
 signal placement_started
 signal placement_finished
 
-func run_placement_phase() -> void:
+func run_placement_phase():
 
 	if active:
 		return
 
 	active = true
+	
+	_align_all_placeables()
 	
 	button = Button.new()
 	button.text = "Finish Placement"
@@ -22,7 +26,28 @@ func run_placement_phase() -> void:
 	canvas.add_child(button)
 	emit_signal("placement_started")
 
-func _button_pressed() -> void:
+func reset_placement():
+	#migrate all placed objects to inactive parent
+	for child in active_holder.get_children():
+		child.reparent(inactive_holder)
+
+func _align_all_placeables():
+	var placement_position = Vector3i(0, 0, 4)
+	
+	for child in inactive_holder.get_children():
+		child.reparent(active_holder)
+		
+	for child in active_holder.get_children():
+		child.global_position = placement_position
+		child.rotation = Vector3.ZERO
+		if (child is Moveable):
+			var m = (child as Moveable)
+			m.rotation_in_degrees = 0
+			placement_position += Vector3i(0, 0, m.grid_size.y)
+		else:
+			placement_position += Vector3i.BACK
+
+func _button_pressed():
 	
 	if not active:
 		return
--- scripts/game_loop_handler.gd
+++ scripts/game_loop_handler.gd
@@ -10,30 +10,45 @@ func _ready() -> void:
 
 func _game_loop() -> void:
 	await get_tree().process_frame
+	await _reset_placement()
+	
 	while true:
 		await _load_random_map()
 		await _wait_for_placement()
 		await _run_wave_phase()
 		await _reward_phase()
+		await _unload_current_map()
+		await _reset_placement()
 		await _run_narrative_popups()
 		
+
+func _unload_current_map() -> void:
+	map_loader.unload_current_map()
+	print("unloaded current map")
+
 func _load_random_map() -> void:
 	map_loader.load_random_map()
+	print("loaded random map")
 
 func _wait_for_placement() -> void:
 	placement_handler.run_placement_phase()
 	await placement_handler.placement_finished
 	print("placement finished")
-
+	
+func _reset_placement() -> void:
+	placement_handler.reset_placement()
+	print("placement reset")
+	
 func _run_wave_phase() -> void:
 	enemy_spawner.spawn_wave(4,1)
 	await EntityHandler.all_enemies_unregistered
+	print("wave cleared reset")
 
 func _reward_phase() -> void:
-	print("Reward phase – TODO: implement real rewards")
+	print("enter Reward phase – TODO: implement real rewards")
 	await get_tree().create_timer(0.5).timeout
 
 
 func _run_narrative_popups() -> void:
-	print("Travel phase – TODO: implement real popups")
+	print("enter Travel phase – TODO: implement real popups")
 	await get_tree().create_timer(0.5).timeout
