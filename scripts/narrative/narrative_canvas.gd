5873ce0e6228db3c5e49782dff3c993753798c1c
LucasBoe <lucas@lumotex.de>
2025-11-21 12:03:48 +0100

    ADDED: type effect

--- scripts/narrative/narrative_canvas.gd
+++ scripts/narrative/narrative_canvas.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var fade_black_color_rect = $Control/ColorRect
 @onready var popup_parent = $Control/NarrativePopup
 
-@onready var narrative_text_label = $Control/NarrativePopup/MarginContainer/VBoxContainer/MarginContainer/Label
+@onready var narrative_text_label = $Control/NarrativePopup/MarginContainer/VBoxContainer/MarginContainer/RichTextLabel
 @onready var choice_button_1 = $Control/NarrativePopup/MarginContainer/VBoxContainer/HBoxContainer/Button1
 @onready var choice_button_2 = $Control/NarrativePopup/MarginContainer/VBoxContainer/HBoxContainer/Button2
 
@@ -11,6 +11,8 @@ var event_choice_buttons : Array[Button]
 
 var narrative_event_folder_path = "res://data/travel/"
 var narrative_event_pool : Array[NarrativeEvent]
+
+var _skip_text_animation = false
 
 signal travel_finished_signal
 
@@ -28,6 +30,11 @@ func _ready() -> void:
 		var event_data = ResourceLoader.load(path)
 		narrative_event_pool.append(event_data)
 
+func _input(event: InputEvent) -> void:
+	# Any mouse button press will trigger a skip
+	if event is InputEventMouseButton and event.pressed:
+		_skip_text_animation = true
+		
 func begin_travel():
 	#pick next event
 	var event : NarrativeEvent = narrative_event_pool.pick_random()
@@ -41,6 +48,7 @@ func _show_event(event):
 	narrative_text_label.text = event.text
 	try_create_button(0, event)
 	try_create_button(1, event)
+	_animate_text(narrative_text_label, event_choice_buttons)
 	
 	#show	
 	visible = true
@@ -56,6 +64,32 @@ func try_create_button(index, event):
 	button.visible = true
 	button.text = choice.button_text	
 	button.pressed.connect(execute_choice.bind(choice))
+	
+func _animate_text(label : RichTextLabel, elements : Array[Button]):
+	
+	_skip_text_animation = false
+	
+	for n in elements:
+		n.visible = false
+		
+	label.visible_characters = 0
+	for c in label.text:
+		
+		if _skip_text_animation:
+			label.visible_ratio = 1
+			break
+		else:
+			label.visible_characters+=1
+		
+		if c == ".":
+			await get_tree().create_timer(.2).timeout
+		else:
+			await get_tree().create_timer(.01).timeout			
+	
+			
+	for n in elements:
+		if not n.text.is_empty():
+			n.visible = true
 
 func execute_choice(choice : EventChoice):
 	
@@ -64,13 +98,11 @@ func execute_choice(choice : EventChoice):
 	
 	#show the consequences of the choice as text
 	if not choice.opt_text.is_empty():
-		narrative_text_label.text = choice.opt_text		
+		narrative_text_label.text = choice.opt_text	
 		choice_button_1.pressed.connect(_end_travel)
 		choice_button_1.text = "continue"
-		
-		choice_button_1.visible = true
-		choice_button_2.visible = false
-		
+		choice_button_2.text = ""	
+		_animate_text(narrative_text_label, event_choice_buttons)
 	#show followup event
 	elif choice.opt_next_event != null:
 		_show_event(choice.opt_next_event)
