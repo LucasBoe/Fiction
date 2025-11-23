b75b30427d3e12539ce744323e298e0d16f39556
LucasBoe <lucas@lumotex.de>
2025-11-23 21:06:49 +0100

    FIXED: scaling effect

--- scripts/utility/juice_util.gd
+++ scripts/utility/juice_util.gd
@@ -5,12 +5,11 @@ static func apply_juice_tween(target: Node3D, type: Tween.TransitionType, durati
 
 	var tween: Tween = target.create_tween()
 
-	var original_scale: Vector3 = target.scale
-	var overshoot_scale: Vector3 = original_scale * 1.05
+	var overshoot_scale: Vector3 = Vector3.ONE * 1.1
 
 	tween.set_ease(Tween.EaseType.EASE_OUT)
 	tween.set_trans(type)
 	tween.tween_property(target, "scale", overshoot_scale, duration * 0.3)
-	tween.tween_property(target, "scale", original_scale, duration * 0.7)
+	tween.tween_property(target, "scale", Vector3.ONE, duration * 0.7)
 	
 	return tween
