fc3d9ca68a693487036cd25667ffe8c5d982cdf6
ollulf <max.bredlau@googlemail.com>
2025-11-28 16:20:12 +0100

    ADDED: Navigation mesh back to project

---
+++ scripts/NavigationMesh.gd
@@ -0,0 +1,16 @@
+extends NavigationRegion3D
+
+@onready var MapLoader = %MapLoader
+signal baked_navigation
+
+func _ready():
+	MapLoader.loaded_map.connect(_on_map_loaded)
+
+func _on_map_loaded():
+	self.bake_navigation_mesh()
+	emit_signal("baked_navigation")
+	print("Navigation Region Rebuild")
+
+func trigger_rebake():
+	await get_tree().process_frame
+	self.bake_navigation_mesh()
