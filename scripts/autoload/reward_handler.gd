2c97cdc0b3a71068db9b5ee4246d7ed280b480b7
LucasBoe <lucas@lumotex.de>
2025-11-28 14:43:36 +0100

    FIXED: reward phase not ending if buildings were destroyed

--- scripts/autoload/reward_handler.gd
+++ scripts/autoload/reward_handler.gd
@@ -85,8 +85,13 @@ func give_rewards():
 	#for building in reward_buildings:
 		#await reward_choice_canvas.show_choice(building.reward)
 		#
-	
-	while reward_triggers.size():
+	while true:
+		# Remove freed / invalid instances
+		reward_triggers = reward_triggers.filter(func(t): return is_instance_valid(t))
+
+		if reward_triggers.is_empty():
+			break
+
 		await get_tree().process_frame
 		
 	await get_tree().create_timer(2).timeout
