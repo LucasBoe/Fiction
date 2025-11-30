a903ec716d8a8d2b8b5d8361b70554693bde592d
LucasBoe <lucas@lumotex.de>
2025-11-30 21:23:44 +0100

    CHANGED: wagon upgrade progression and default wagons in wagon maker

--- scripts/supply_ui.gd
+++ scripts/supply_ui.gd
@@ -1,9 +1,9 @@
 extends Control
 
-@onready var amount_label = $MarginContainer/MarginContainer2/HBoxContainer/Label
+@onready var amount_label = $MarginContainer/HBoxContainer/MarginContainer/Label
 
 func _ready():
 	MoneyHandler.money_changed_signal.connect(on_money_changed)
 	
 func on_money_changed(amount_after):
-	amount_label.text = str(roundi(amount_after),"$")
+	amount_label.text = str(roundi(amount_after))
