46fe8f37bb90feceae16755cb25a0fa9cbe8281d
LucasBoe <lucas@lumotex.de>
2025-12-01 17:06:18 +0100

    ADDED: rotate wagon line to tutorial ui

--- scripts/tutorial_ui.gd
+++ scripts/tutorial_ui.gd
@@ -6,6 +6,7 @@ class_name TutorialUI
 var move_camera
 var zoom_in_out
 var toggle_top
+var rotate_wagon
 var place_kings
 var place_others
 var finish_place
@@ -17,6 +18,7 @@ func _ready():
 	move_camera = create_line("Use WASD to move the camera")
 	zoom_in_out = create_line("Use the mouse wheel to zoom in / out")
 	toggle_top = create_line("Use TAB to toggle top down camera")
+	rotate_wagon = create_line("Use SPACE or RMB to rotate wagons")
 	place_kings = create_line("Place Kings Wagon at safe location")
 	place_others = create_line("Place defensive Wagons")
 	finish_place = create_line("Finish placement")
