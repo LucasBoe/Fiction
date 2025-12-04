9073db6ed572d7e712d98d25a7ff6f41c53dba4d
LucasBoe <lucas@lumotex.de>
2025-12-04 13:12:18 +0100

    ADDED: loading text to fade canvas

--- scripts/autoload/fade_effect_canvas.gd
+++ scripts/autoload/fade_effect_canvas.gd
@@ -1,11 +1,15 @@
 extends CanvasLayer
 
 @onready var fade_black_color_rect: ColorRect = $Fade
+@onready var loading_label = $Label
 const DURATION : float = 1.0
 
 signal finished_signal
 
 var fade_tween: Tween
+
+func _ready():
+	loading_label.hide()
 
 func fade_in():
 	await fade_to(1)
@@ -20,6 +24,7 @@ func fade_in_out():
 func fade_to(opacity: float) -> void:
 	# Clamp opacity between 0 and 1
 	opacity = clamp(opacity, 0.0, 1.0)
+	loading_label.visible = opacity > .5
 
 	# Kill any previous fade tween
 	if fade_tween != null and is_instance_valid(fade_tween):
